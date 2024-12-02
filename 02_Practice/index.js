d24e4c99622bf0e97ae01d1f83bff18456be7097
IDDQD93 <ggteamsasha@gmail.com>
2024-12-02 19:18:22 +0500

    Add files via upload

---
+++ 02_Practice/index.js
@@ -0,0 +1,9 @@
+const currentYear = 2024
+
+let studentName = "Илья"
+let year = "2006"
+let age = currentYear - parseInt(year)
+
+console.log("Студент:", studentName);
+console.log("Год рождения:", year);
+console.log("Возраст:", age);
